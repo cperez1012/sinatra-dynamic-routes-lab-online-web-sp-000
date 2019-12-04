4a303d378ba8a633a41e2a7722a06d78db1140dc
Christian Perez <perezchristian1012@gmail.com>
2019-12-04 05:49:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get '/reversename/:name' do
+    @reverse_name = params[:name]
+    @reverse_name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    erb :say
+  end
+
+  get '/:operation/:number1/:number2' do
+
+  end
 end
